6d02afe6095f2fc91c680ef9da4d1df3139e5a11
cen14 <nelson.christopher.e@gmail.com>
2016-03-22 11:22:58 -0400

    Update detect.py

--- detect.py
+++ detect.py
@@ -17,6 +17,11 @@ def detect_problems(filename):
         print 'Seems OK!'
         
 filenames = glob.glob('/Users/dcl9/gcbCourse/materials/cshl_rna_seq/*.bed*')
+#H   H EEEEE L     L      OOO       W   W  OOO  RRRR  L     DDDD  !!
+#H   H E     L     L     O   O      W W W O   O R   R L     D   D !! 
+#HHHHH EEEEE L     L     O   O      W W W O   O RRRR  L     D   D !! 
+#H   H E     L     L     O   O  ,,   W W  O   O R   R L     D   D    
+#H   H EEEEE LLLLL LLLLL  OOO  ,,    W W   OOO  R   R LLLLL DDDD  !!
 for f in filenames:
     print f
     detect_problems(f)
